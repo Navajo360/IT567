fcdde5ec412f9d1968e86d3540a11091830bfe04
Buck Porter <dporter360@gmail.com>
2017-10-20 17:01:17 -0600

    Multiple hosts

--- ps1.py
+++ ps1.py
@@ -2,31 +2,31 @@
 from socket import *
 import sys
 import time
-from datetime import *
 from optparse import OptionParser
-import re
 
 #Variables
 targetHost = ''
 targetPort = ''
 maxPortNum = 9999
 minPortNum = 1
-tp=[]
+tp = []
+th = []
 
-#Command-line switches to specify a host and port(s)
+#Command-line switches to specify host(s) and port(s)
 parser = OptionParser()
 parser.add_option("-t", "--target", help="target host address")
 parser.add_option("-p", "--port", help="target port")
 (options, args) = parser.parse_args()
 if not options.target:
-	parser.error("Target host not given (-t [Target Host])")
+	parser.error("Target host(s) not given (-t [Target Host],[Target Host])")
 if not options.port:
 	parser.error("Target port(s) not given (-p [Port Number],[Port Number]")
 
 #Scanner function
-def myScanner(targetHost, targetPort, code = 1):
+def myTCPscanner(targetHost, targetPort, code = 1):
 	try:
 		connection = socket(AF_INET, SOCK_STREAM)
+		connection.settimeout(2)
 		myScannerCode = connection.connect_ex((targetHost, targetPort))
 		if myScannerCode == 0:
 			code = myScannerCode
@@ -42,18 +42,25 @@ targetPort = options.port
 for i in targetPort.split(','):
 	#print i
 	tp += [int(i)]
-#print tp
 
-print("Host: %s Port(s): %s" % (targetHost, targetPort))
-print("Port scan started\n")
+#Seperates the hosts since they are added in a comma seperated list
+for j in targetHost.split(','):
+	th += [j]
 
-for ports in tp:
-	try:
-		response = myScanner(targetHost, ports)
+print("Host(s): %s \nPort(s): %s" % (th, tp))
+print("\nPort scan started at %s" % (time.strftime("%H:%M:%S")))
 
-		if response == 0:
-			print("Port %d: \tOPEN" % (ports))
-		else:
-			print("Port %d: \tCLOSED" % (ports))
-	except Exception, e:
-		pass
+for hosts in th:
+	print("\nHost: %s" % (hosts))
+	for ports in tp:
+		try:
+			response = myTCPscanner(hosts, ports)
+
+			if response == 0:
+				print("Port %d: \tOPEN" % (ports))
+			else:
+				print("Port %d: \tCLOSED" % (ports))
+		except Exception, e:
+			pass
+
+print("\nPort scan finshed at %s - %s" % (time.strftime("%H:%M:%S")))
